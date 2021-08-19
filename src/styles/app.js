6f95955ee0880d62fc0e92d428f422efdfbce45e
John Obertubbesing <oberthewhat@gmail.com>
2021-08-19 10:51:47 -0500

    changed background to cover

--- src/styles/app.js
+++ src/styles/app.js
@@ -4,7 +4,7 @@ export const styles = {
         width: '105vw',
         background: `linear-gradient(to right, #066e9eba, #d8dadbba), url("https://images.unsplash.com/photo-1604605091198-77815622c9ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80") `,
         opacity: '.8',
-        backgroundSize: 'fill',
+        backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
 
     },
