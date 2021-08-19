ce5e01222f839445300380547c039cb0a8826faf
John Obertubbesing <oberthewhat@gmail.com>
2021-08-19 02:37:09 -0500

    some rendering adjustments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { styles } from "./styles/app"
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
+
+  const [width, setWindowWidth] = useState(0)
+
+   useEffect(() => { 
+
+     updateDimensions();
+
+     window.addEventListener('resize', () => updateDimensions);
+     return () => 
+       window.removeEventListener('resize', updateDimensions);
+    }, [])
+
+    const updateDimensions = () => {
+      const width = window.innerWidth
+      setWindowWidth(width)
+    }
+
+    const reactive = width > 600 ? styles.underConstruction : styles.mobileUnderConstruction
+
   return (
     <div style={styles.tempBody}>
-        <div style={styles.underConstruction}>
-         <div>ThrillNerds.com Under Construction</div>
-         <CircularProgress />
+        <div style={reactive}>
+         <div>ThrillNerds.com</div>
+         <div>Under Construction</div>
+         <div style={styles.spinner}>
+         <CircularProgress/>
+         </div>
          <div style={styles.checkBack}>Check back soon!</div>
         </div>
 
--- src/styles/app.js
+++ src/styles/app.js
@@ -1,12 +1,11 @@
 export const styles = {
     tempBody: {
-        height: '100vh',
-        width: '100vw',
+        height: '105vh',
+        width: '105vw',
         background: `linear-gradient(to right, #066e9eba, #d8dadbba), url("https://images.unsplash.com/photo-1604605091198-77815622c9ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80") `,
         opacity: '.8',
-        backgroundSize: 'cover',
+        backgroundSize: 'fill',
         backgroundRepeat: 'no-repeat',
-        padding: 0
 
     },
     underConstruction: {
@@ -19,9 +18,22 @@ export const styles = {
         color: 'black',
         fontFamily: 'DejaVu Sans Mono, monospace',
         flexDirection: 'column'
-
     },
     checkBack: {
         fontSize: '25px'
+    },
+    mobileUnderConstruction: {
+        fontSize: '1.5em',
+        display: 'flex',
+        height: '50vh',
+        width: '100vw',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'black',
+        fontFamily: 'DejaVu Sans Mono, monospace',
+        flexDirection: 'column'
+    },
+    spinner: {
+        margin: '6px'
     }
 }
